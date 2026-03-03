bcc1308e3c67594adbef880617bf7ec824009159
Adil1122 <adil7090@gmail.com>
2026-03-03 14:56:06 +0500

    London Strategy Centre avatar voice and context 2

--- apps/demo/app/api/secrets.ts
+++ apps/demo/app/api/secrets.ts
@@ -7,7 +7,7 @@ export const AVATAR_ID = "910777e7-530e-47cb-bd29-b3661ca8a74f";
 export const IS_SANDBOX = false;
 
 // FULL MODE Customizations
-// London Strategy Centre's avatar voice and context
+// London Strategy Centre avatar voice and context
 export const VOICE_ID = "9064923c-28c1-49d8-a9df-63915f83b61b";
 export const CONTEXT_ID = "ceaed12f-72d8-431e-9d9d-2ab9c5b965f7";
 export const LANGUAGE = "en";
